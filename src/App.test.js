2e11e1120044023e33eb5caa102f51f6ac15a07a
Sarah Rimron-Soutter <sarah@teaandbiscuits.net>
2019-05-19 15:47:52 +0100

    getting a feel for react-testing-library

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, Simulate, wait, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+afterEach(cleanup);
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('renders', () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it('has a header', () => {
+    const { container } = render(<App />);
+    expect(container.querySelectorAll('header').length).toEqual(1);
+    expect(container.querySelectorAll('h1').length).toEqual(1);
+  });
+  describe('programmeTable', () => {
+    it('has the columns id, name, description, active ', () => {
+      const { getByText } = render(<App />);
+      expect(getByText('ID')).toBeInTheDocument();
+      expect(getByText('Name')).toBeInTheDocument();
+      expect(getByText('Description')).toBeInTheDocument();
+      expect(getByText('Active')).toBeInTheDocument();
+    });
+  });
 });
