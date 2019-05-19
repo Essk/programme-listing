4c8b0cd01e9bafbea6bb3da52594cff267885bd2
Sarah Rimron-Soutter <sarah@teaandbiscuits.net>
2019-05-19 13:24:46 +0100

    Sketching out App structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import programmes from './programmes.json';
+import { DisplayList } from './classes/displayList';
 
 function App() {
+  const displayList = new DisplayList(programmes);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Awesome Programme List</h1>
       </header>
+      <main>
+        <section id="search">
+          <p>search goes here</p>
+        </section>
+        <section id="data">
+          <table className="programmes">
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Active</th>
+              <th>Actions</th>
+            </tr>
+            {displayList.programmeList.map(programme => (
+              <tr>
+                <td>{programme.id}</td>
+                <td>{programme.name}</td>
+                <td>{programme.description}</td>
+                <td>{programme.active ? 'yes' : 'no'}</td>
+                <td>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </table>
+        </section>
+      </main>
     </div>
   );
 }
