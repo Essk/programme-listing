74da91d23347f4821a056c4cf45ec68707862eda
Sarah Rimron-Soutter <sarah@teaandbiscuits.net>
2019-05-19 14:53:02 +0100

    continue building App structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import './App.css';
 import programmes from './programmes.json';
+import { ReactComponent as EditIcon } from './edit-solid.svg';
+import { ReactComponent as CrossIcon } from './times-solid.svg';
 import { DisplayList } from './classes/displayList';
-
+const ProgrammeTable = props => (
+  <table className="programmes">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Active</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>{props.children}</tbody>
+  </table>
+);
+const ProgrammeRow = props => (
+  <tr className={props.programme.active ? 'active' : 'inactive'}>
+    <td>{props.programme.id}</td>
+    <td>{props.programme.name}</td>
+    <td>{props.programme.description}</td>
+    <td>{props.programme.active ? 'yes' : 'no'}</td>
+    <td className="row-actions">
+      <button className="row-action row-action--edit">
+        <EditIcon />
+        Edit
+      </button>
+      <button className="row-action row-action--delete">
+        <CrossIcon /> Delete
+      </button>
+    </td>
+  </tr>
+);
 function App() {
   const displayList = new DisplayList(programmes);
   return (
@@ -15,31 +47,29 @@ function App() {
           <p>search goes here</p>
         </section>
         <section id="data">
-          <table className="programmes">
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Active</th>
-              <th>Actions</th>
-            </tr>
+          <ProgrammeTable programmeList={displayList.programmeList}>
             {displayList.programmeList.map(programme => (
-              <tr>
-                <td>{programme.id}</td>
-                <td>{programme.name}</td>
-                <td>{programme.description}</td>
-                <td>{programme.active ? 'yes' : 'no'}</td>
-                <td>
-                  <button>Edit</button>
-                  <button>Delete</button>
-                </td>
-              </tr>
+              <ProgrammeRow programme={programme} key={programme.id} />
             ))}
-          </table>
+          </ProgrammeTable>
         </section>
       </main>
+      <footer>
+        Attribution: Icons used are from Font Awesome in accordance with the
+        terms of their
+        <a
+          target="_blank"
+          href="https://fontawesome.com/license"
+          rel="noopener noreferrer"
+        >
+          licence
+        </a>
+      </footer>
     </div>
   );
 }
 
+/**
+
+ */
 export default App;
