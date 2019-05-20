7ac6980c99f6f8b56b82bbb3a59626c2e7a0c90f
Sarah Rimron-Soutter <sarah@teaandbiscuits.net>
2019-05-20 22:09:29 +0100

    enhancement: tweaking styles for better legibility

--- src/components/programmeRow.js
+++ src/components/programmeRow.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ReactComponent as EditIcon } from '../assets/pencil.svg';
 import { ReactComponent as CrossIcon } from '../assets/x.svg';
+import { ReactComponent as CheckIcon } from '../assets/check.svg';
+import { ReactComponent as CircleSlashIcon } from '../assets/circle-slash.svg';
 import { ActionButton } from './actionButton';
 
 export const ProgrammeRow = props => (
@@ -8,7 +10,10 @@ export const ProgrammeRow = props => (
     <td>{props.programme.id}</td>
     <td>{props.programme.name}</td>
     <td>{props.programme.description}</td>
-    <td>{props.programme.active ? 'yes' : 'no'}</td>
+    <td className="cell-active">
+      {props.programme.active ? <CheckIcon /> : <CircleSlashIcon />}
+      <span>{props.programme.active ? 'Yes' : 'No'}</span>
+    </td>
     <td className="row-actions">
       <ActionButton
         type="edit"
